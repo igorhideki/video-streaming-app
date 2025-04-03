a365f2ac004ce917e5dbeccf75dbb6fe3ad4c41e
Igor Trindade <igorhideki@gmail.com>
2025-04-03 19:22:08 -0400

    add about page

--- apps/web/src/views/AboutView.vue
+++ apps/web/src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p class="mb-2 text-base/relaxed">
+    <h1 class="mb-4 text-2xl font-bold text-neutral-900 dark:text-neutral-100">About project</h1>
+    <p class="mb-6 text-base/relaxed">
       This project plays a video provided by an API via streaming, allowing users to interact with
       the content through annotations and indicators.
     </p>
@@ -10,10 +11,10 @@
     </p>
 
     <ul class="text-base-relaxed list-disc pl-6">
-      <li>Frontend development with Vue.js</li>
-      <li>API consumption</li>
-      <li>Video streaming</li>
-      <li>Manipulation of graphic elements with the Canvas API</li>
+      <li class="mb-2">Frontend development with Vue.js</li>
+      <li class="mb-2">API consumption</li>
+      <li class="mb-2">Video streaming</li>
+      <li class="mb-2">Manipulation of graphic elements with the Canvas API</li>
     </ul>
   </div>
 </template>
