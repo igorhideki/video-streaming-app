69192e84a91ca428ed3293da859bb467e828afcb
Igor Trindade <igorhideki@gmail.com>
2025-04-03 18:56:45 -0400

    add about page

--- apps/web/src/views/AboutView.vue
+++ apps/web/src/views/AboutView.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    <p class="text-base">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ipsa unde necessitatibus
-      nulla dolorem saepe aliquid recusandae deleniti dicta laborum.
+    <p class="mb-2 text-base/relaxed">
+      This project plays a video provided by an API via streaming, allowing users to interact with
+      the content through annotations and indicators.
     </p>
+
+    <p class="mb-2 text-base/relaxed">
+      The main goal of this project is to put the following knowledge into practice:
+    </p>
+
+    <ul class="text-base-relaxed list-disc pl-6">
+      <li>Frontend development with Vue.js</li>
+      <li>API consumption</li>
+      <li>Video streaming</li>
+      <li>Manipulation of graphic elements with the Canvas API</li>
+    </ul>
   </div>
 </template>
